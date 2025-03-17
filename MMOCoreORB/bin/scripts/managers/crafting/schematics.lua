5f569786760afe4835aa33598d8134bff3bf3ebe
Unknown Author <none@example.com>
2025-03-17 02:27:39 +0000

    Removing unnecessary iff

--- MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
+++ MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
@@ -1839,6 +1839,5 @@ schematics = {
 	{path="object/draft_schematic/structure/bespin_house_schem.iff"},
 	--new weapons
 	{path="object/draft_schematic/weapon/lance_staff_cryo.iff"},
-	{path="object/draft_schematic/weapon/lightsaber/lightsaber_polearm_s1.iff"},
 	{path="object/draft_schematic/weapon/carbine_geo.iff"},
 };
