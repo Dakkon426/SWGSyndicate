0b0964acbdeed549044636f85c5bc098b7d7ed2e
Unknown Author <none@example.com>
2025-03-21 06:59:11 +0000

    Change jedi xp loss logic, change abilities, instant factories, xp buffed for ent and craft, krayt pearl changes

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -1202,7 +1202,27 @@ void PlayerManagerImplementation::killPlayer(TangibleObject* attacker, CreatureO
 	player->updateTimeOfDeath();
 	player->clearBuffs(true, false);
 
+	// If the player is a Jedi and died in PvE (not killed by a player), make them lose Jedi XP
 	PlayerObject* ghost = player->getPlayerObject();
+	if (ghost != nullptr && ghost->getJediState() >= 2 && (!attacker->isPlayerCreature() && !attacker->isPet())) {
+		int jediXpCap = ghost->getXpCap("jedi_general");
+		int xpLoss = (int)(jediXpCap * -0.5f);
+		int curExp = ghost->getExperience("jedi_general");
+
+		int negXpCap = -10000000; // Cap on negative jedi experience
+
+		if ((curExp + xpLoss) < negXpCap)
+			xpLoss = negXpCap - curExp;
+
+		if (xpLoss < -2000000)
+			xpLoss = -2000000;
+
+		awardExperience(player, "jedi_general", xpLoss, true);
+		StringIdChatParameter message("base_player", "prose_revoke_xp");
+		message.setDI(xpLoss * -1);
+		message.setTO("exp_n", "jedi_general");
+		player->sendSystemMessage(message);
+	}
 
 	if (ghost != nullptr) {
 		ghost->resetIncapacitationTimes();
@@ -1620,7 +1640,7 @@ void PlayerManagerImplementation::sendPlayerToCloner(CreatureObject* player, uin
 	// Jedi experience loss.
 	if (ghost->getJediState() >= 2) {
 		int jediXpCap = ghost->getXpCap("jedi_general");
-		int xpLoss = (int)(jediXpCap * -0.15);
+		int xpLoss = (int)(jediXpCap * -1.f);
 		int curExp = ghost->getExperience("jedi_general");
 
 		int negXpCap = -10000000; // Cap on negative jedi experience
